fcd320ccd119925e7d2c9bff3fb835cfab0bde5a
Craxulator <craxulator@gmail.com>
2025-01-16 14:03:44 -0500

    Add files via upload
    Signed-off-by: Craxulator <craxulator@gmail.com>

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-function CalculateZeroOffset(Radius, Angle)
-{
-    let X = (Radius / 2) * Math.cos(Angle - Math.PI / 2)
-    let Y = (Radius / 2) - ((Radius / 2) * Math.sin(Angle - Math.PI / 2) + Radius / 2)
-    return new Vector2(X, Y)
-}
-
 function Main() 
 {
-    let SpotLight = document.getElementsByTagName("spotlight")[0]
-    let Base = document.getElementsByTagName("spotlight-base")[0]
+    let SpotLights = [...document.getElementsByTagName("spotlight")]
+    let SpotLightCount = 0
+    SpotLights.forEach(SpotLight => {
+        SpotLight.style.left = `${SpotLightCount * 20 + 7}vw`
+        SpotLightCount += 1
+    })
+
+    UpdateSpotLights(SpotLights, new Vector2(10, 10))
 
     document.addEventListener('mousemove', (Event) => {
-        let BaseRect = SpotLight.getBoundingClientRect()
-        let Rect = SpotLight.getBoundingClientRect()
-        let Radius = Rect.height / 2
-        let X = Event.x - (BaseRect.left + BaseRect.width / 2); let Y = window.innerHeight - Event.y
-        let Angle = Math.atan(X / Y)
-        Angle = Math.clamp(Angle, -0.25, 0.25)
-        // console.log({X : X, Y : Y, Angle : Angle})
-        let Offset = CalculateZeroOffset(Radius, Angle)
-        SpotLight.style.transform = `translate(${Offset.X}px, ${Offset.Y}px) rotate(${Angle}rad)`
-    });
-};
+        UpdateSpotLights(SpotLights, Event)
+    })
+    
+    document.addEventListener('scroll', (Event) => {
+        SpotLights.forEach(SpotLight => {
+            SpotLight.style.bottom = `${-window.scrollY}px`
+        })
+    })
+}
 
 document.onreadystatechange = Main;
-
-/* Auto Refresh //
-setTimeout(function() {
-    location.reload();
-}, 250);
-//*/
